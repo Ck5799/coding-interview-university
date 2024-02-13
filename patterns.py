aaa3d69ba1758503d69381229a462d52f7325953
Chaitanya Kilambi <82971206+Ck5799@users.noreply.github.com>
2024-02-13 14:33:16 -0800

    made changes to patterns

--- patterns.py
+++ patterns.py
@@ -66,12 +66,70 @@ def halfdiamond(rows):
         print("*"*(rows-i))    
     print("\n")
 
-def UpsidedownTriangleStar(rows):
+def binarytriangle(rows):
+    string=""
+    for i in range (rows):
+        if i%2==0:
+            string="1 "+string
+        else:
+            string="0 "+string
+        print(string)
+    print("\n")
+        
+def numtriangle(rows):
+    string1=""
+    string2=""
+    string3=""
     for i in range(rows):
-        print((" "+"*"*(rows-(i+1))+" "*((2*(i+1))-1)+"*"*(rows-(i+1))+" "))
+        string1=string1+str(i+1)
+        string2=" "*(2*(rows-i)-1)
+        string3=str(i+1)+string3
+        print(string1+string2+string3)
+    print("\n")
+
+def ascendingtriange(rows):
+    count = 0
+    for i in range(rows+1):
+        string=""
+        for j in range(i):
+            count+=1
+            string=string+str(count)+" "
+        print(string)    
+
+
+def alphatriangle(rows):
     for i in range(rows):
-        print(""+"*"*((i+1))+" "*(2*(rows-i)-1)+"*"*((i+1))+"") 
-    print("\n")  
+       charecter=chr(ord("A")+i)
+       print(charecter*(i+1))
+
+
+def alphatriangle_1(rows):
+    charecter=""
+    for i in range(rows):
+       charecter=charecter+chr(ord("A")+i)
+       print(charecter)
+
+def inveredalpha(rows):
+    charecter=""
+    for i in range(rows+1):
+       print(charecter)
+       charecter=""
+       for j in range(rows-i):
+        charecter=charecter+chr(ord('A')+j)+" "
+        
+    
+    
+    
+        
+
+
+
+# def UpsidedownTriangleStar(rows):
+#     for i in range(rows):
+#         print((" "+"*"*(rows-(i+1))+" "*((2*(i+1))-1)+"*"*(rows-(i+1))+" "))
+#     for i in range(rows):
+#         print(""+"*"*((i+1))+" "*(2*(rows-i)-1)+"*"*((i+1))+"") 
+#     print("\n")  
 
 if __name__ == '__main__':
     pattern_1 = boxstar(5)
@@ -84,4 +142,9 @@ if __name__ == '__main__':
     pattern_8=UpsidedownTriangleStars(5)
     pattern_9=diamond(5)
     pattern_10=halfdiamond(5)
-    pattern_11=UpsidedownTriangleStar(5)
+    pattern_11=binarytriangle(5)
+    pattern_12=numtriangle(5)
+    pattern_13=alphatriangle(5)
+    pattern_14=alphatriangle_1(5)
+    pattern_15=ascendingtriange(5)
+    pattern_16=inveredalpha(5)
